32268c0d09271af890e34d3b0f3df621065c0557
Mark <150802654+MarkChang2023@users.noreply.github.com>
2024-02-22 16:07:47 +0000

    modify

--- lesson7_3.py
+++ lesson7_3.py
@@ -9,7 +9,7 @@ print("==========猜數字遊戲==========")
 while True:
     keyin = pyip.inputInt(f"請猜數字範圍{min}~{max}:")
     count += 1
-    print(keyin )
+    print(keyin)
     if keyin == random_number:
         print(f"賓果!猜對了,答案是:{random_number}")
         print(f"您猜了{count}次")
